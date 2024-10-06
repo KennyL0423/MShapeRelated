58fe72116e03db0c1a0db4c064d3f7288310b0ee
suyx1999 <suyx1999@gmail.com>
2024-10-06 20:08:58 +0800

    Updated data info

--- java/src/main/java/org/Main.java
+++ java/src/main/java/org/Main.java
@@ -5,37 +5,54 @@ import org.util.DataLoader;
 import java.io.IOException;
 import java.util.List;
 import org.util.Metric;
+
+enum DATASET{
+    AIR("air", 166, 3),
+    ECG("ecg", 140, 5),
+    WAFER("Wafer", 152, 2),
+    PATTERN("TwoPatterns", 128, 4),
+    ELEC("ElectricDevices", 96, 7),
+    FACES("FacesUCR", 131, 14),
+    ADIAC("Adiac", 176, 37);
+
+
+    int seqLen;
+    int clusterNum;
+    String fileName;
+    DATASET(String fileName, int seqLen, int clusterNum){
+        this.fileName = fileName;
+        this.seqLen = seqLen;
+        this.clusterNum = clusterNum;
+    }
+}
 public class Main {
-
-    static int seqLen = 140;
-    static int clusterNum = 5;
-
-    static String dataset = "ecg";
 
     static String method = "frok";
 
 
     public static void main(String[] args) throws IOException {
-        String csvFile = "../datasets/time_series/" + dataset + ".csv";
+        DATASET dataset = DATASET.WAFER;
 
-        int iter = 10;
+        String csvFile = "../datasets/time_series/" + dataset.fileName + ".csv";
+
+        int iter = 1;
         long start = System.currentTimeMillis();
         double[] RIarray = new double[iter];
 
         for (int i = 0; i < iter; i++) {
-            List<double[]> timeSeriesData = DataLoader.readTimeSeriesFromCSV(csvFile, seqLen);
+            List<double[]> timeSeriesData = DataLoader.readTimeSeriesFromCSV(csvFile, dataset.seqLen);
             int[] pred;
             switch (method){
                 case "wfcm":
-                    wFCM wfcm = new wFCM(timeSeriesData, seqLen, clusterNum);
+                    wFCM wfcm = new wFCM(timeSeriesData, dataset.seqLen, dataset.clusterNum);
                     pred = wfcm.fit();
                     break;
                 case "frok":
-                    FrOKShape frok = new FrOKShape(timeSeriesData, seqLen, clusterNum, 0.6);
+                    FrOKShape frok = new FrOKShape(timeSeriesData, dataset.seqLen, dataset.clusterNum, 0.6);
                     pred = frok.fit();
                     break;
                 case "pam":
-                    PAM pam = new PAM(timeSeriesData, seqLen, clusterNum);
+                    PAM pam = new PAM(timeSeriesData, dataset.seqLen, dataset.clusterNum);
                     pred = pam.fit();
                     break;
                 default:
